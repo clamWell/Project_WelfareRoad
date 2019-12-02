5ebc8fbe0cdd9ab1e787beba4aea4261bf3e3db1
h62446244! <pepesi258@hanmail.net>
2019-12-02 15:08:58 +0900

    [Project Welfare Road] 두번째 커밋 - 테스트 끝, 실제 퍼블리싱 시작 전 백업

--- js/custom.js
+++ js/custom.js
@@ -1,97 +1,55 @@
-(function () {
-	var ieTest = false,
-		screenWidth = $(window).width(),
-		screenHeight = $(window).height();
+$(window).load(function() {
 
-	var controller = new ScrollMagic.Controller();
+  var ieTest = false,
+    screenWidth = $(window).width(),
+    screenHeight = $(window).height(),
+    imgURL = "http://img.khan.co.kr/spko/storytelling/2019/labordeath/",
+    isMobile = screenWidth <= 710 && true || false,
+    isNotebook = (screenWidth <= 1300 && screenHeight < 750) && true || false;
 
-	var horizon_tween_1 = TweenMax.to(".horizon-dimension-1", 0.5, {
-								left: -( $(".horizon-dimension-1").width()-screenWidth )
-						   });
-	var horizon_tween_2 = TweenMax.to(".horizon-dimension-2", 0.5, {
-								left: -( $(".horizon-dimension-2").width()-screenWidth )
-						   });
-	var horizon_tween_3 = TweenMax.to(".horizon-dimension-3", 0.5, {
-								left: -( $(".horizon-dimension-3").width()-screenWidth )
-						   });
-	var horizon_tween_4 = TweenMax.to(".horizon-dimension-4", 0.5, {
-								left: -( $(".horizon-dimension-4").width()-screenWidth )
-						   });
+  var agent = navigator.userAgent.toLowerCase();
+  if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
+    ieTest = true;
+  } else {
+    ieTest = false;
+  }
 
-	
-	var b_h = 300;
-	new ScrollMagic.Scene({
-				triggerElement: $(".actual-scroll")[0],
-				triggerHook: "onEnter",
-				offset: $(".dimension-holder").offset().top,
-				duration: $(".actual-scroll").height()-$(".dimension-holder").offset().top
-			}).setTween(horizon_tween_1).addTo(controller).addIndicators();
-			
-	new ScrollMagic.Scene({
-				triggerElement: $(".actual-scroll")[0],
-				triggerHook: "onEnter",
-				offset: $(".dimension-holder").offset().top,
-				duration: $(".actual-scroll").height()-$(".dimension-holder").offset().top
-			}).setTween(horizon_tween_2).addTo(controller).addIndicators();
+  window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+  };
 
-	new ScrollMagic.Scene({
-				triggerElement: $(".actual-scroll")[0],
-				triggerHook: "onEnter",
-				offset: $(".dimension-holder").offset().top,
-				duration: $(".actual-scroll").height()-$(".dimension-holder").offset().top
-			}).setTween(horizon_tween_3).addTo(controller).addIndicators();
-		
-	new ScrollMagic.Scene({
-				triggerElement: $(".actual-scroll")[0],
-				triggerHook: "onEnter",
-				offset: $(".dimension-holder").offset().top,
-				duration: $(".actual-scroll").height()-$(".dimension-holder").offset().top
-			}).setTween(horizon_tween_4).addTo(controller).addIndicators();
+  $(window).resize(function() {
+    screenWidth = $(window).width();
+    screenHeight = $(window).height();
+  });
 
 
-    new ScrollMagic.Scene({
-			duration: $(".actual-scroll").height()-$(".dimension-holder").offset().top,
-			offset: $(".dimension-holder").offset().top,
-			triggerHook: "onEnter"
-		}).setPin(".dimension-holder").addTo(controller).addIndicators();
+  $("body").removeClass("fixed");
+  $(".loading-page").fadeOut(function() {
 
-	for(i=0; i<3; i++){
-		$info = $(".info-layer .info-board").find(".each-item");
-		new ScrollMagic.Scene({
-		    triggerElement: $(".info-trigger")[i],
-			duration: 500,
-			offset: $(".dimension-holder").offset().top,
-			triggerHook: "onEnter"
-		}).setTween(TweenMax.to( $info.eq(i), 0.5, {
-								display: "block",
-								opacity: 1,
-								scale: 1
-						   })).addTo(controller);	
-	}
-  
+  });
 
 
-  /*
-	var scene1 = new ScrollMagic.Scene({
-      triggerElement: "#trigger1", 
-            triggerHook: 0.8
-    })
-    .setClassToggle("#animate1", "visible")
-    .addTo(controller)
-    .addIndicators();
-    
-    var revealElements = document.getElementsByClassName("animation2");
-    for (var i=0; i<revealElements.length; i++) {
-        
-        var scene2 = new ScrollMagic.Scene({
-            triggerElement: revealElements[i],
-            offset: 50,
-            triggerHook: 0.9
-        })
-        .setClassToggle(revealElements[i], "visible") // add class toggle
-        .addTo(controller)
-        .addIndicators({name: "(box) " + (i+1), colorStart:"#F6B352", colorTrigger:"#F6B352"});
-    }
-    */
 
-}())
+  $(window).scroll(function() {
+    var nowScroll = $(window).scrollTop();
+
+
+  });
+
+});
+
+// 공유
+
+function sendSns(sns) {
+  var url = encodeURIComponent(location.href),
+    txt = encodeURIComponent($("title").html());
+  switch (sns) {
+    case 'facebook':
+      window.open('http://www.facebook.com/sharer/sharer.php?u=' + url);
+      break;
+    case 'twitter':
+      window.open('http://twitter.com/intent/tweet?text=' + txt + '&url=' + url);
+      break;
+  }
+}
