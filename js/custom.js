a08e028a287a4cc25f3091e9c7957188d85243af
h62446244! <pepesi258@hanmail.net>
2019-12-02 17:01:33 +0900

    [Project Welfare Road] 세번째 커밋 - 퍼블리싱 진행중 - 기본 틀 구성 - 시작 애니메이션

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,66 @@
 $(window).load(function() {
 
-  var ieTest = false,
-    screenWidth = $(window).width(),
-    screenHeight = $(window).height(),
-    imgURL = "http://img.khan.co.kr/spko/storytelling/2019/labordeath/",
-    isMobile = screenWidth <= 710 && true || false,
-    isNotebook = (screenWidth <= 1300 && screenHeight < 750) && true || false;
+	var ieTest = false,
+		screenWidth = $(window).width(),
+		screenHeight = $(window).height(),
+		imgURL = "http://img.khan.co.kr/spko/storytelling/2019/labordeath/",
+		isMobile = screenWidth <= 710 && true || false,
+		isNotebook = (screenWidth <= 1300 && screenHeight < 750) && true || false;
 
-  var agent = navigator.userAgent.toLowerCase();
-  if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
-    ieTest = true;
-  } else {
-    ieTest = false;
-  }
+	var agent = navigator.userAgent.toLowerCase();
+	if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
+		ieTest = true;
+	} else {
+		ieTest = false;
+	}
 
-  window.onbeforeunload = function() {
-    window.scrollTo(0, 0);
-  };
+	window.onbeforeunload = function() {
+		window.scrollTo(0, 0);
+	};
 
-  $(window).resize(function() {
-    screenWidth = $(window).width();
-    screenHeight = $(window).height();
-  });
+	$(window).resize(function() {
+		screenWidth = $(window).width();
+		screenHeight = $(window).height();
+	});
+
+	function settingDimenstionWidth(){
+		var scrollDetph = $(".actual-scroll").height();
+		$(".horizon-dimension-1").width(scrollDetph);
+		$(".horizon-dimension-2").width(scrollDetph*0.5);
+		$(".horizon-dimension-3").width(scrollDetph*0.25);
+		$(".horizon-dimension-4").width(scrollDetph*0.1);
+	}
+	settingDimenstionWidth();
+
+	function ShowCharacterBox(){
+		$(".character-holder").stop().animate({"bottom":"20%"}, 1000, "swing", function(){});
+	}; 
+
+	//afterLoad
+	function afterLoad(){
+		$("body").removeClass("fixed");
+		$(".horizon-dimension").animate({"top":"0%"}, 1200, "easeInCubic");
+		$(".loading-page").animate({"position":"absolute", "top":"-100%"}, 1200, "easeInCubic", function(){
+			 ShowCharacterBox();
+		});
+	}
+	afterLoad();	
+	
+
+	chrSpreadDiv = document.getElementById("characterAni");
+	function defaultCharcterFrame(){
+		chrSpreadDiv.style.left = "0px"
+	};
+
+	var shiftRobbyFrameTimer = setInterval(function() { shiftRobbyFrame() }, 200)
 
 
-  $("body").removeClass("fixed");
-  $(".loading-page").fadeOut(function() {
 
-  });
-
+	$(window).scroll(function() {
+		var nowScroll = $(window).scrollTop();
 
 
-  $(window).scroll(function() {
-    var nowScroll = $(window).scrollTop();
-
-
-  });
+	});
 
 });
 
