bf03353f606ca6d0f296d7f95bc60bdabc54721d
h62446244! <pepesi258@hanmail.net>
2019-12-03 16:34:29 +0900

    [Project Welfare Road] 다섯번째 커밋 - 퍼블리싱 진행중 * 배경 레이어 수평 움직임 구현 완료 * 스크롤 값에 따른 애니메이션 트리깅 테스트

--- js/custom.js
+++ js/custom.js
@@ -23,8 +23,12 @@ $(window).load(function() {
 		screenHeight = $(window).height();
 	});
 
-	function settingDimenstionWidth(){
-		var scrollDetph = $(".actual-scroll").height();
+	var scrollDetph = $(".actual-scroll").height();
+	var $chrSpreadDiv = $("#characterAni"),
+		$scrContainer = $(".scroll-animate-area"),
+		$dimensionHorizon = $(".horizon-dimension");
+
+	function settingDimenstionWidth(){		
 		$(".horizon-dimension-1").width(scrollDetph);
 		$(".horizon-dimension-2").width(scrollDetph*0.5);
 		$(".horizon-dimension-3").width(scrollDetph*0.25);
@@ -33,10 +37,10 @@ $(window).load(function() {
 	settingDimenstionWidth();
 
 	function ShowCharacterBox(){
-		$(".character-holder").stop().animate({"bottom":"20%"}, 1000, "swing", function(){
-			//chrAniTimer = setInterval(function() { makeChrAni() }, 200);
+		$(".character-holder").stop().animate({"bottom":"20%"}, 1000, "swing", function(){		
 			defaultCharcterFrame();
 			canScroll = true; 
+			canChrAni = true; 
 		});
 	}; 
 
@@ -50,68 +54,7 @@ $(window).load(function() {
 		});
 	}
 	afterLoad();	
-	
-
-	var $chrSpreadDiv = $("#characterAni");
-	function defaultCharcterFrame(){
-		$chrSpreadDiv.css("left", "0px");
-	};
-	var canScroll = false; 
-	var chrWidth = 150;
-	var chrStart, chrEnd, counter = 0;
-	var chrAniTimer;
-
-	function chrFall(){
-	  $chrSpreadDiv.css("left", "-450px");
-	};
-
-	function makeChrAni() {
-		$chrSpreadDiv.css("left", (-1 * chrWidth * counter)  + "px");
-		if(counter>=2){
-			counter = 0;
-		}else{
-			counter++;
-		}					
-	};
-
-	function clearChrAniTimer() {
-	  clearInterval(chrAniTimer);
-	};
-
-	function scrollAct(){
 		
-	};
-
-	var dimensionSpeed = new Array;
-	function setLayerSpeed(){
-	 
-	  ///// 여기까지 12.02  
-
-	};
-
-	//일단 디폴트로 설정해둠
-	var layersMovement = "horizontal";
-	var dimensionHorizon = $(".horizon-dimension");
-
-	function moveLayers() {
-		if (layersMovement == "horizontal") {
-			for (var e = 0; e < dimensionHorizon.length; e++)  dimensionHorizon[e].css("left", -1 * dimensionSpeed[e] * VP + "px"; ); 
-		}else{
-		
-		}
-	}
-
-//orientRobby(), checkRobbyJumpFallSwim(), moveLayers(), shiftUpDownHorizontalLayers(), animateInformationAndEnemiesElements(), animateRobbyRunSwim(), //hideScrollOrSwipeTextContainer(), hideContactConfirmationContainer(), deviceFunctionScrollSwipe(), printScrollSwipeText()
-
-	$(window).scroll(function() {
-		var nowScroll = $(window).scrollTop();
-		if(canScroll==true){
-			detectPageVerticalPosition();
-			scrollAct();
-		}
-
-	});
-
 	var preVP, VP = 0; 
 	function detectPageVerticalPosition(){
 		preVP = VP;
@@ -121,8 +64,114 @@ $(window).load(function() {
 			if( VP = pageVerticalPositionOnTouch + (touchStartX - touchCurrentX) < 0){
 				VP = 0
 			}	  
-	    }
+	    }		
 	}
+
+
+	function defaultCharcterFrame(){
+		$chrSpreadDiv.css("left", "0px");
+	};
+	var canScroll,canChrAni = false; 
+	var chrWidth = 150;
+	var chrStart, chrEnd, counter = 0;
+	var chrAniTimer;
+
+	function chrFall(){
+	  $chrSpreadDiv.css("left", "-450px");
+	};
+
+	function chrRun() {
+		$chrSpreadDiv.css("left", (-1 * chrWidth * counter)  + "px");
+		if(counter>=2){
+			counter = 0;			
+		}else{
+			counter++;
+		}			
+	};
+	
+	function makeChrRun(){
+		if(canChrAni ==true){	
+			canChrAni = false;
+			clearChrAniTimer();
+			chrAniTimer = setInterval(function() { chrRun() }, 200);
+		}
+	};
+
+	function clearChrAniTimer() {
+	  clearInterval(chrAniTimer);
+	};
+
+	function scrollAct(){
+		moveLayers();
+		//makeChrRun();
+		animateObject();
+	};
+
+	var dimensionSpeed = new Array;
+	function setLayerSpeed(){
+		for (var i = 0; i < $dimensionHorizon.length; i++) {
+			var widthGap =  $dimensionHorizon.eq(i).width() - screenWidth;
+			var speedFactor = widthGap / ( scrollDetph - screenWidth ) ;
+			dimensionSpeed.push(speedFactor)
+		}
+	};
+	setLayerSpeed();
+	
+	//일단 디폴트를 수평이동으로 설정해둠
+	var layersMovement = "horizontal";
+
+	function moveLayers() {
+		if (layersMovement == "horizontal") {
+			for (var e = 0; e < $dimensionHorizon.length; e++){ 
+				$dimensionHorizon.eq(e).css("left", (-1 * dimensionSpeed[e] * VP) + "px" );
+			}
+		}else{
+		
+		}
+	}
+
+	//var bank = $(".bank-sign");
+	var $aniObs = $(".aniOb");
+	var bankAniDone = false;
+	function animateObject(){
+		if (layersMovement == "horizontal"){
+			for(a=0;a<$aniObs.length;a++){
+				var aniObsStartPos = $aniObs.eq(a).position().left;
+					aniObsEndPos = $aniObs.eq(a).position().left + $aniObs.eq(a).width();
+				/*
+				 if( (preVP_trigger < aniObsStartPos || preVP_trigger > aniObsEndPos ) && (VP_trigger > aniObsStartPos) && (VP_trigger < aniObsEndPos) && ( $aniObs.eq(a) == $(".bank-sign") ) && (bankAniDone == false) ){
+					  animateBank(),
+					  bankAniDone = true;
+				  } */
+				if( (VP + screenWidth*0.7 > aniObsStartPos) && (VP + screenWidth*0.7 < aniObsEndPos) && ( $aniObs.eq(a).hasClass("bank-sign") ) && (bankAniDone == false) ){
+					  animateBank(),
+					  bankAniDone = true;
+				} 
+			}		
+		}		
+	};
+
+		 
+	function animateBank(){
+		$(".bank-sign").animate({"top":"-15%"}, 800, "easeOutElastic");
+		$(".bank-sign-front img").addClass("rotate");
+
+	};
+
+// 스크롤 때 마다 실행하는 함수들
+//orientRobby(),  > 캐릭터 너비세팅
+//checkRobbyJumpFallSwim(),moveLayers(),
+//shiftUpDownHorizontalLayers(), animateInformationAndEnemiesElements(), animateRobbyRunSwim(), //hideScrollOrSwipeTextContainer(), hideContactConfirmationContainer(), deviceFunctionScrollSwipe(), printScrollSwipeText()
+
+	$(window).scroll(function() {
+		var nowScroll = $(window).scrollTop();
+		if(canScroll==true){
+			detectPageVerticalPosition();
+			scrollAct();
+		}
+
+	});
+	
 
 	
 
