b956d7d4e43d50b4681506d574c52175c93ff247
h62446244! <pepesi258@hanmail.net>
2019-12-09 18:22:05 +0900

    [Project Welfare Road] 스크립트 수정 * 비행기 구간 수직+수평 이동 스크립트 전격 수정 by. 경상선배

--- js/custom.js
+++ js/custom.js
@@ -16,75 +16,38 @@ $(function(){
 
 	var deviceName = navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i) ? "iosdevice" : navigator.userAgent.match(/Android/i) ? "android" : navigator.userAgent.match(/BlackBerry/i) ? "blackberry" : navigator.userAgent.match(/IEMobile/i) ? "iemobile" : navigator.userAgent.match(/Silk/i) ? "kindle" : "computer"
 
-	window.onbeforeunload = function() {
-		window.scrollTo(0, 0);
-	}
-
-	// 스크롤, 디멘션 세팅(S)
-	var scrollDetph = $(".actual-scroll").height();
 	var $chrSpreadDiv = $(".character-spread"),
+		scrollDetph = $(".actual-scroll").height();
 		$scrContainer = $(".scroll-animate-area"),
-		$dimensionHorizon = $(".horizon-dimension");
+		$dimensionHorizon = $(".horizon-dimension"),
+		dimensionSpeed = new Array;
 
-	function settingDimenstionWidth(){
-		$(".horizon-dimension-1").width(scrollDetph); // 근경
-		$(".horizon-dimension-2").width(scrollDetph*0.5); // 근-원경
-		$(".horizon-dimension-3").width(scrollDetph*0.25); // 원경
-		$(".horizon-dimension-4").width(scrollDetph*0.1);// 초원경
-		$(".horizon-dimension-5").width(scrollDetph); // 근경 중 캐릭터 위로 덮는 요소들
-	}
-	settingDimenstionWidth();
+	var objectUpdown = [], oc = 0;
+	var verticalUpdown = { v: 0, m: 0, p: 0, call:"" };
 
-	function ShowCharacterBox(){ // 시작하면 캐릭터 하늘에서 떨어짐
-		$(".character-holder").stop().animate({"bottom":"20%"}, 1000, "swing", function(){
-			defaultCharcterFrame();
-			canScroll = true;
-			canChrAni = true;
-		});
-	}
-	// 스크롤, 디멘션 세팅(E)
+	var $changePoint = $(".change-point");
+	var nowChrStage = 0;
 
-	//afterLoad
-	function afterLoad(){
-		$("body").removeClass("fixed");
-		$(".horizon-dimension").animate({"top":"0%"}, 1200, "easeInCubic");
-		$(".loading-page").animate({"position":"absolute", "top":"-100%"}, 1200, "easeInCubic", function(){
-			chrFall();
-			ShowCharacterBox();
-		});
-	}
+	var moneyWidth = 150;
+	var canMoneyFly = true;
+	var moneyFlyTimer;
+	var moneyCounter = 0;
 
-	// 현재 스크롤 위치 감지하여 변수에 담기
-	var preVP, deltaVP, VP = 0;
-	var vertical_p;
+	var $aniObs = $(".aniOb");
+	var bankAniDone = false;
+	var nowSendingHeart = false;
+	var workBuildingArrive = false;
+	var nowElevator = false;
+	var weddingPlaneGoUp = false;
+	var weddingPlaneGoDown = false;	
+	var weddingPlaneComplete = false;
+	var honeymoonPops = false;
+	var houseHeartDone = false;
+	var drugGiven = false;
+	var babyCry = false; 
+	var upDownWidth;
+	var oStartP, oEndP, vStartP, vEndP, updownSwitcher;
 
-	function detectPageVerticalPosition(){
-
-		if (layersMovement == "vertical"){
-
-			console.log(VP);
-			//window.scrollTo(vertical_p, 0);
-			VP = vertical_p;
-
-		} else if (layersMovement == "horizontal"){
-
-			preVP = VP;
-			if (deviceName == "computer"){ // PC
-				VP = (ieTest == true) ? document.documentElement.scrollTop : pageYOffset // ie 9 이하는 scrollTop 아니면 pageYOffset
-			} else { //모바일
-				if( VP = pageVerticalPositionOnTouch + (touchStartX - touchCurrentX) < 0){
-					VP = 0
-				}
-		    }
-			deltaVP = VP - preVP;
-			if (VP <= 0){
-				//  초기위치 리셋용
-			}
-
-		}
-	}
-
-	////// 캐릭터 뛰는 동작 (S)
 	var chrStatus = "run";
 	var canScroll,canChrAni = false;
 	var defaultchrWidth = 150;
@@ -92,13 +55,36 @@ $(function(){
 	var chrStart, chrEnd, counter = 0;
 	var chrAniTimer;
 	var switcher = 1;
+
+
 	var VP_when_moving1, VP_when_moving2;
+	var preVP, deltaVP, VP = 0;
+	var vertical_p;
 
-	/*function orientChr(){
-		if (deltaVP > 0) $chrSpreadDiv.css("top", "0px"); /// 눈 깜박임 추후 추가
-		if (deltaVP < 0) $chrSpreadDiv.css("top", "-175px");
-	}*/
+	var layersMovement = "horizontal"; //일단 디폴트를 수평이동으로 설정해둠
 
+	/////  스크롤, 디멘션 세팅(S)  /////
+	function settingDimenstionWidth(){
+		$(".horizon-dimension-1").width(scrollDetph); // 근경
+		$(".horizon-dimension-2").width(scrollDetph*0.5); // 근-원경
+		$(".horizon-dimension-3").width(scrollDetph*0.25); // 원경
+		$(".horizon-dimension-4").width(scrollDetph*0.1);// 초원경
+		$(".horizon-dimension-5").width(scrollDetph); // 근경 중 캐릭터 위로 덮는 요소들
+	}
+
+	// 시작하면 캐릭터 하늘에서 떨어짐
+	function ShowCharacterBox(){
+		$(".character-holder").stop().animate({"bottom":"20%"}, 1000, "swing", function(){
+			defaultCharcterFrame();
+			canScroll = true;
+			canChrAni = true;
+		});
+	}
+	/////  스크롤, 디멘션 세팅(E)  /////
+
+	/////  캐릭터 뛰는 동작 (S)  /////
+
+	// 캐릭터 방향 전환
 	function orientChr(){
 		var $orientChr, topValue;
 		if (nowChrStage == 4){
@@ -123,7 +109,7 @@ $(function(){
 	}
 
 	//캐릭터 움직이도록 프레임변경
-	function chrRun() {	
+	function chrRun() {
 		var chrWidth = nowChrStage == 5 ? 200 : defaultchrWidth;
 		if (chrStatus == "run")	chrStart = chrRunStartFrame, chrEnd = chrRunEndFrame;
 
@@ -154,18 +140,9 @@ $(function(){
 		canChrAni = true;
 
 	}
-	////// 캐릭터 뛰는 동작 (E)
+	////// 캐릭터 뛰는 동작 (E)  //////
 
-	function scrollAct(){
-		checkChrBoxState();
-		makeChrRun();
-		moveLayers();
-		animateObject();
-		orientChr();
-	};
-
-	////// 화면 이동 (S)
-	var dimensionSpeed = new Array;
+	////// 화면 이동 (S)  /////
 	function setLayerSpeed(){ // 각 근~원경별로 레이어 움직이는 스피드 지정
 		for (var i = 0; i < $dimensionHorizon.length; i++) {
 			var widthGap =  $dimensionHorizon.eq(i).width() - screenWidth;
@@ -173,35 +150,125 @@ $(function(){
 			dimensionSpeed.push(speedFactor)
 		}
 	};
-	setLayerSpeed();
 
-	var layersMovement = "horizontal"; //일단 디폴트를 수평이동으로 설정해둠
+	// 현재 스크롤 위치 감지
+	function detectPageVerticalPosition(){
+
+		// 수직이동 - 애니메이션 처리의 경우
+		if (layersMovement == "vertical"){
+
+			VP = vertical_p;
+
+		// 수평이동 혹은 배경이나 물체 수직이동
+		} else if (layersMovement == "horizontal" || layersMovement == "verticalUpdown" || layersMovement == "objectUpdown"){
+
+			preVP = VP;
+			if (deviceName == "computer"){ // PC
+				VP = (ieTest == true) ? document.documentElement.scrollTop : pageYOffset; // ie 9 이하는 scrollTop 아니면 pageYOffset
+			} else { //모바일
+				if( VP = pageVerticalPositionOnTouch + (touchStartX - touchCurrentX) < 0){
+					VP = 0
+				}
+		    }
+			deltaVP = VP - preVP;
+			if (VP <= 0){
+				//  초기위치 리셋용
+			}
+
+		}
+	}
 
 	function moveLayers() {
+
 		if (layersMovement == "vertical") VP = vertical_p;
+
+		if (weddingPlaneGoDown == true){
+			vStartP = verticalUpdown.e;
+			vEndP = verticalUpdown.s;
+			updownSwitcher = -1;
+		} else {
+			vStartP = verticalUpdown.s;
+			vEndP = verticalUpdown.e;
+			updownSwitcher = 1;
+		}
+
+		console.log(layersMovement);
+		console.log(weddingPlaneGoDown);
+		if (layersMovement == "objectUpdown"){
+
+			console.log("oc : " + oc);
+			if (weddingPlaneGoDown == true){
+				oStartP = objectUpdown[oc].e;
+				oEndP = objectUpdown[oc].s;
+			} else {
+				oStartP = objectUpdown[oc].s;
+				oEndP = objectUpdown[oc].e;
+			}
+
+			if (deltaVP > 0){
+
+				if ((updownSwitcher * objectUpdown[oc].p > 0 && oEndP < objectUpdown[oc].m) || (updownSwitcher * objectUpdown[oc].p < 0 && oEndP > objectUpdown[oc].m)){
+					objectUpdown[oc].o.css("top", oEndP + "px");
+					layersMovement = objectUpdown[oc].call;
+					if (objectUpdown.length != oc + 1) oc++;
+					return;
+				}
+
+			} else if (deltaVP < 0){
+
+				if ((updownSwitcher * objectUpdown[oc].p > 0 && oStartP > objectUpdown[oc].m) || (updownSwitcher * objectUpdown[oc].p < 0 && oStartP < objectUpdown[oc].m)){
+					objectUpdown[oc].o.css("top", oStartP + "px");
+					layersMovement = objectUpdown[oc].start;
+					if (oc > 0) oc--;
+					return;
+				}
+
+			}
+
+			objectUpdown[oc].m += objectUpdown[oc].p * updownSwitcher * deltaVP;
+			objectUpdown[oc].o.css("top", objectUpdown[oc].m + "px");
+
+		}
+
+		if (layersMovement == "verticalUpdown"){
+
+			if (deltaVP > 0){
+
+				if ((updownSwitcher * verticalUpdown.p > 0 && vEndP < verticalUpdown.m) || (updownSwitcher * verticalUpdown.p < 0 && vEndP > verticalUpdown.m)){
+					layersMovement = verticalUpdown.call;
+					$(".horizon-dimension").css("top", vEndP + "px");
+					return;
+				}
+
+
+			} else if (deltaVP < 0){
+				if ((updownSwitcher * verticalUpdown.p > 0 && vStartP > verticalUpdown.m) || (updownSwitcher * verticalUpdown.p < 0 && vStartP < verticalUpdown.m)){
+					layersMovement = verticalUpdown.start;
+					$(".horizon-dimension").css("top", vStartP + "px");
+					return;
+				}
+
+			}
+
+			console.log("vm : " + verticalUpdown.m);
+			console.log("vs : " + vStartP);
+
+			verticalUpdown.m += verticalUpdown.p * updownSwitcher * deltaVP;
+
+			$(".horizon-dimension").css("top", verticalUpdown.m + "px");
+		}
 
 		for (var e = 0; e < $dimensionHorizon.length; e++){
 			$dimensionHorizon.eq(e).css("left", (-1 * dimensionSpeed[e] * VP) + "px" );
 		}
 
 	}
-	////// 화면 이동 (E)
+	////// 화면 이동 (E)  //////
 
-	var $aniObs = $(".aniOb");
-	var bankAniDone = false;
-	var nowSendingHeart = false;
-	var workBuildingArrive = false;
-	var nowElevator = false;
-	var weddingPlaneGoUp = false;
-	var weddingPlaneGoDown = false;
-	var honeymoonPops = false;
-	var houseHeartDone = false;
-	var drugGiven = false;
-	var babyCry = false; 
-
+	///// 애니메이션 처리 /////
 	function animateObject(){
 		if (layersMovement == "horizontal"){
-			for(a=0;a<$aniObs.length;a++){
+			for(a = 0; a < $aniObs.length; a++){
 				var aniObsStartPos = $aniObs.eq(a).position().left;
 					aniObsEndPos = $aniObs.eq(a).position().left + $aniObs.eq(a).width();
 				//뱅크 애니메이션
@@ -219,22 +286,94 @@ $(function(){
 				}else if( (VP + $(".character-holder").position().left <= aniObsEndPos) && $aniObs.eq(a).hasClass("meet-husband")){
 					$(".husband .husband-holder img").css({"left":"0px"});
 				}
+
 				// 뱅기 모드 시작
-				if( $aniObs.eq(a).hasClass("workbuilding-area") && (VP + $(".character-holder").position().left  > aniObsEndPos) && ( VP + $(".character-holder").position().left  < $(".wedding-photo-area").position().left ) && weddingPlaneGoUp == false ){
-					weddingPlaneGoUp = true; 
-					console.log("허니문떠남")
-					$(".character-box-plane").addClass("plane-rotate-up");										
+				if ( $aniObs.eq(a).hasClass("workbuilding-area") && (VP + $(".character-holder").position().left  > aniObsEndPos) && weddingPlaneGoUp == false && weddingPlaneComplete == false){
+
+					weddingPlaneGoUp = true;
+					upDownWidth = ($(".wedding-photo-area").position().left + ($(".wedding-photo-area").width() / 2)) - aniObsEndPos;
+					console.log("허니문떠남");
+
+					objectUpdown = [];
+					objectUpdown.push({
+						start: "horizontal",
+						o: $(".character-box-plane"),
+						s: 0,
+						e: -350,
+						m: 0,
+						p: -1 * (350 / upDownWidth),
+						call: "objectUpdown"
+					});
+					objectUpdown.push({
+						start: "objectUpdown",
+						o: $(".character-box-plane"),
+						s: -350,
+						e: 0,
+						m: -350,
+						p: 350 / upDownWidth,
+						call: "verticalUpdown"
+					});
+
+					verticalUpdown.start = "objectUpdown";
+					verticalUpdown.s = 1140;
+					verticalUpdown.e = 0;
+					verticalUpdown.m = 1140;
+					verticalUpdown.p = -1 * (1140 / ($changePoint.eq(4).position().left - 500 - aniObsEndPos - upDownWidth * 2));
+					verticalUpdown.call = "horizontal";
+
+					layersMovement = "objectUpdown";
+
+				} else if ( $aniObs.eq(a).hasClass("workbuilding-area") && (VP + $(".character-holder").position().left <= aniObsEndPos) && ( weddingPlaneGoUp == true || weddingPlaneGoDown == true || weddingPlaneComplete == true) ){
+					weddingPlaneGoUp = false;
+					weddingPlaneComplete = false;
+					weddingPlaneGoDown = false;
 				}
-				//신혼 앨범 만나면 하트, 폭죽
+
+				if ($aniObs.eq(a).hasClass("workbuilding-area") && VP + $(".character-holder").position().left <= $changePoint.eq(4).position().left && weddingPlaneGoDown == false && weddingPlaneGoUp == true && weddingPlaneComplete == true) {
+
+					weddingPlaneGoDown = true;
+					upDownWidth = ($(".wedding-photo-area").position().left + ($(".wedding-photo-area").width() / 2)) - aniObsEndPos;
+					console.log("허니문돌아옴")
+
+					objectUpdown = [];
+					objectUpdown.push({
+						start: "horizontal",
+						o: $(".character-box-plane"),
+						s: -350,
+						e: 0,
+						m: -350,
+						p: 350 / upDownWidth,
+						call: "objectUpdown"
+					});
+					objectUpdown.push({
+						start: "objectUpdown",
+						o: $(".character-box-plane"),
+						s: 0,
+						e: -350,
+						m: 0,
+						p: -1 * (350 / upDownWidth),
+						call: "verticalUpdown"
+					});
+
+					verticalUpdown.start = "objectUpdown";
+					verticalUpdown.s = 0;
+					verticalUpdown.e = 1140;
+					verticalUpdown.m = 0;
+					verticalUpdown.p = 1140 / ($changePoint.eq(4).position().left - 500 - aniObsEndPos - upDownWidth * 2);
+					verticalUpdown.call = "horizontal";
+					layersMovement = "verticalUpdown";
+
+				} else if ($aniObs.eq(a).hasClass("workbuilding-area") && VP + $(".character-holder").position().left <= $changePoint.eq(4).position().left && weddingPlaneGoDown == true && weddingPlaneGoUp == true && weddingPlaneComplete == true){
+					layersMovement = "verticalUpdown";
+				}
+
+				//// 신혼앨범에 들어가는 순간 아래 함수 호출되게 해주세요
+				/*				
+				//신혼 앨범 만나면 하트, 폭죽  
 				if( $aniObs.eq(a).hasClass("wedding-photo-area") && ( VP + $(".character-holder").position().left  > aniObsStartPos) && honeymoonPops == false ){
 					AnimateHoneymoonPops();										
-				}
-				// 신혼 앨범 떠나면 바닥으로
-				if( $aniObs.eq(a).hasClass("wedding-photo-area") && ( VP + $(".character-holder").position().left  > aniObsEndPos) && weddingPlaneGoDown == false ){
-					weddingPlaneGoDown = true;
-					console.log("허니문끝 내려가기 시작")
-					$(".horizon-dimension").stop().animate({"top":"0%"}, 1200, "swing");								
-				}
+				}*/
+
 				// 행복주택 지나갈때 하트
 				if( $aniObs.eq(a).hasClass("honeymoon-house-area") && ( VP + $(".character-holder").position().left > aniObsStartPos+400) && houseHeartDone == false ){
 					houseHeart();
@@ -253,7 +392,7 @@ $(function(){
 					$(".babycry img").animate({"width":"230px","opacity":"1"}, 800, "easeOutElastic"); 	
 
 				}
-				
+
 
 			}
 
@@ -293,7 +432,7 @@ $(function(){
 		nowElevator = true;
 		console.log("빌딩내려가기");
 		$(".horizon-dimension").stop().animate({"top":"0%"}, 1200, "swing",function(){
-			$("html, body").scrollTop($(".workbuilding-area").position().left-$(".character-holder").position().left-200);
+			$("html, body").scrollTop($(".workbuilding-area").position().left-$(".character-holder").position().left-300);
 			$("body").removeClass("fixed");
 			nowElevator = false;
 			layersMovement = "horizontal";
@@ -312,10 +451,7 @@ $(function(){
 		$(".bank-sign-front img").addClass("rotate");
 	};
 
-	var moneyWidth = 150;
-	var canMoneyFly = true;
-	var moneyFlyTimer;
-	var moneyCounter = 0;
+	// 날개 파닥파닥
 	function moneyfly() {
 		if(canMoneyFly ==false){
 			$(".flying-money-slider").css("left", 0);
@@ -334,8 +470,6 @@ $(function(){
 			moneyFlyTimer = setInterval(function() { moneyfly() }, 200);
 		}
 	};
-	// 은행 애니메이션
-
 
 	// 남편 만나는 애니메이션
 	function sendHeart(){
@@ -345,7 +479,7 @@ $(function(){
 			$heart.eq(h).delay(300 * h).animate({"width": "30px", "top":"0", "left":"-100px","opacity":"0"}, 700, "swing");
 		}
 	};
-	// 남편 만나는 애니메이션
+
 
 	// 신혼 폭죽, 하트
 	function AnimateHoneymoonPops(){
@@ -368,18 +502,15 @@ $(function(){
 	// 신혼집 애니메이션
 
 
-	// 캐릭터 스테이지 구분 체크
-	var $changePoint = $(".change-point");
-	var nowChrStage = 0;
-	
+	///// 캐릭터 스테이지마다 바뀌는 부분 구분 체크 /////
 	function hideChrBoxforChange(){
 		$(".character-holder .character-box").hide();
 		$(".character-holder .character-spread").hide();
 	};
 
 	function changeChrBox(n){
+
 		if( nowChrStage == n){ // 스테이지 같음 아무런 액션 하지 않음
-			
 		}else if( nowChrStage !== n ){ //스테이지 바뀜
 			nowChrStage = n;
 			console.log(n+"번째 스테이지");
@@ -396,6 +527,7 @@ $(function(){
 			}else if(n==4){ // 비행기
 				$(".character-holder .character-box-plane").show();
 			}else if(n==5){ // 신혼여행 끝
+				weddingPlaneComplete = true;
 				$(".character-holder .character-box-wedding").show();
 				$(".character-holder .character-box-wedding .character-spread").show();
 			}else if(n==6){ // 신혼집 이후 아내 임신
@@ -416,7 +548,7 @@ $(function(){
 			}else if(n==9){ // 출산 이후 남편 출근, 아내 혼자 남음
 				$(".character-holder .character-box-normal").show();
 				$(".character-holder .character-spread-j").show();
-			}				
+			}	
 		}
 
 	};
@@ -432,10 +564,30 @@ $(function(){
 				if( chrPos >= $changePoint.eq(c).position().left && chrPos <$changePoint.eq(c+1).position().left){
 					changeChrBox(c+1);
 				}
-			}				
-		} 
+			}
+		}
 	};
-	// 캐릭터 스테이지 구분 체크
+	////// 캐릭터 스테이지 구분 체크  /////
+
+	function afterLoad(){
+		$("body").removeClass("fixed");
+		$(".horizon-dimension").animate({"top":"0%"}, 1200, "easeInCubic");
+		$(".loading-page").animate({"position":"absolute", "top":"-100%"}, 1200, "easeInCubic", function(){
+			chrFall();
+			ShowCharacterBox();
+		});
+	}
+
+	function scrollAct(){
+		checkChrBoxState();
+		makeChrRun();
+		moveLayers();
+		animateObject();
+		orientChr();
+	};
+
+	settingDimenstionWidth();
+	setLayerSpeed();
 
 	$(window).on("load", function(){
 		afterLoad();
@@ -448,9 +600,9 @@ $(function(){
 		} else {
 			window.scrollTo(0, 0);
 		}
+	}).on("onbeforeunload" , function(){
+		window.scrollTo(0, 0);
 	});
-
-
 
 
 });
